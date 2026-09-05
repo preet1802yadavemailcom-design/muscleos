727c4eeae3ff50744bcdb30352a78211021bf611
preet1802yadavemailcom-design <preet1802yadavemail.com@gmail.com>
2026-09-05 08:54:16 +0530

    add missing FirebaseAdminService file

--- backend/src/shared/logger.module.ts
+++ backend/src/shared/logger.module.ts
@@ -4,10 +4,11 @@ import { AuditService } from './services/audit.service';
 import { EncryptionService } from './services/encryption.service';
 import { ExportService } from './services/export.service';
 import { LoggerService } from './services/logger.service';
+import { FirebaseAdminService } from './services/firebase-admin.service';
 
 @Global()
 @Module({
-  providers: [LoggerService, EncryptionService, AuditService, ExportService],
-  exports: [LoggerService, EncryptionService, AuditService, ExportService],
+  providers: [LoggerService, EncryptionService, AuditService, ExportService, FirebaseAdminService],
+  exports: [LoggerService, EncryptionService, AuditService, ExportService, FirebaseAdminService],
 })
 export class LoggerModule {}
